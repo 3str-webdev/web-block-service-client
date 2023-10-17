4d96c20c3335e722a316192e651c8f8d3369d8f5
3str-webdev <3str.webdev@gmail.com>
2023-10-17 21:07:22 +0300

    feat: add auto redirecting from BFF to server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Решаем проблему с CORS. Настраиваем редиректы с BFF на сервер
+  rewrites() {
+    return [
+      {
+        source: "/api/:path",
+        destination: "http://localhost:3000/:path",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
