d265c6cb4e69947b269488543ce82740bb39fed3
3str-webdev <3str.webdev@gmail.com>
2023-10-19 14:33:16 +0300

    feat: add ui-header component in uikit

---
+++ src/shared/uikit/ui-header.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import { HTMLAttributes, ReactNode } from "react";
+import { UiLogo } from "./ui-logo";
+
+export type UiHeaderProps = HTMLAttributes<HTMLElement> & {
+  content?: ReactNode;
+};
+
+export const UiHeader = ({ className, content, ...props }: UiHeaderProps) => {
+  return (
+    <header
+      {...props}
+      className={clsx(
+        className,
+        "px-4 py-2 border-b border-purple-700 flex items-center justify-between bg-slate-50"
+      )}
+    >
+      <div className="flex items-center gap-1">
+        <UiLogo className="w-12 h-12" />
+        <h1 className="text-2xl font-semibold">WBS</h1>
+      </div>
+      {content}
+    </header>
+  );
+};
