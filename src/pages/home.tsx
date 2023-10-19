ff47275a4f0161be04f33fbd4589eb7643911b27
3str-webdev <3str.webdev@gmail.com>
2023-10-19 23:31:55 +0300

    feat: add protection on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,19 @@
+import { useSessionQuery } from "@/entities/session";
+import { SignOutButton } from "@/features/auth";
+import { UiHeader } from "@/shared/uikit/ui-header";
+
 export function HomePage() {
-  return <></>;
+  const { data } = useSessionQuery();
+  return (
+    <>
+      <UiHeader
+        contentElement={
+          <div className="flex items-center gap-2">
+            {data?.email}
+            <SignOutButton />
+          </div>
+        }
+      />
+    </>
+  );
 }
