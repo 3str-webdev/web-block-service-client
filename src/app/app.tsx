d8915d10b8b97fc241e4b2e580fa138d7e1aa8ec
3str-webdev <3str.webdev@gmail.com>
2023-10-19 14:32:22 +0300

    feat: add global font

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from "next/app";
 import { AppProviders } from "./app-providers";
 
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 export function App({ Component, pageProps }: AppProps) {
   return (
     <AppProviders>
-      <Component {...pageProps} />;
+      <div className={montserrat.className}>
+        <Component {...pageProps} />
+      </div>
     </AppProviders>
   );
 }
