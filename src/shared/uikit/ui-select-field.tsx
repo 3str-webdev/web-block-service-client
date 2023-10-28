72e7211dafd0f009b0b447440196c40c06552dbc
3str-webdev <3str.webdev@gmail.com>
2023-10-28 14:35:39 +0300

    feat: add forwardRef for ui-input and ui-select for working with react-hook-form

--- src/shared/uikit/ui-select-field.tsx
+++ src/shared/uikit/ui-select-field.tsx
@@ -1,39 +1,38 @@
 import clsx from "clsx";
-import { SelectHTMLAttributes, useId } from "react";
+import { SelectHTMLAttributes, forwardRef, useId } from "react";
 
-export type UiSelectOption = {
-  value: string;
+export type UiSelectOption<T = string> = {
+  value: T;
   label: string;
 };
 export type UiSelectFieldProps = SelectHTMLAttributes<HTMLSelectElement> & {
   options?: UiSelectOption[];
 };
 
-export const UiSelectField = ({
-  className,
-  options,
-  ...props
-}: UiSelectFieldProps) => {
-  const id = useId();
-  const getSelectOptionKey = (index: number): string => {
-    return `select_${id}_option_${index}`;
-  };
-  return (
-    <select
-      {...props}
-      className={clsx(
-        className,
-        "rounded border border-slate-300 transition-colors outline-none px-2 h-10",
-        "focus:border-purple-700"
-      )}
-    >
-      {options?.map((option, i) => {
-        return (
-          <option key={getSelectOptionKey(i)} value={option.value}>
-            {option.label}
-          </option>
-        );
-      })}
-    </select>
-  );
-};
+export const UiSelectField = forwardRef<HTMLSelectElement, UiSelectFieldProps>(
+  ({ className, options, ...props }, ref) => {
+    const id = useId();
+    const getSelectOptionKey = (index: number): string => {
+      return `select_${id}_option_${index}`;
+    };
+    return (
+      <select
+        {...props}
+        ref={ref}
+        className={clsx(
+          className,
+          "rounded border border-slate-300 transition-colors outline-none px-2 h-10",
+          "focus:border-purple-700"
+        )}
+      >
+        {options?.map((option, i) => {
+          return (
+            <option key={getSelectOptionKey(i)} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
+    );
+  }
+);
--- src/shared/uikit/ui-text-field.tsx
+++ src/shared/uikit/ui-text-field.tsx
@@ -1,17 +1,20 @@
 import clsx from "clsx";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
 
 export type UiTextFieldProps = InputHTMLAttributes<HTMLInputElement>;
 
-export const UiTextField = ({ className, ...props }: UiTextFieldProps) => {
-  return (
-    <input
-      {...props}
-      className={clsx(
-        className,
-        "rounded border border-slate-300 transition-colors outline-none px-2 h-10",
-        "focus:border-purple-700"
-      )}
-    />
-  );
-};
+export const UiTextField = forwardRef<HTMLInputElement, UiTextFieldProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        {...props}
+        ref={ref}
+        className={clsx(
+          className,
+          "rounded border border-slate-300 transition-colors outline-none px-2 h-10",
+          "focus:border-purple-700"
+        )}
+      />
+    );
+  }
+);
